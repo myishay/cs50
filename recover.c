37bb996ba1eb477cf920c66920dee317dbf8e949
myishay <myishay@gmail.com>
2019-11-08 15:15:32 +0200

    Update recover.c

--- recover.c
+++ recover.c
@@ -39,79 +39,8 @@ int main ()
        fread(&block, 512, 1, inp);
        if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && block[3] >= 0xe0 && block[3] <= 0xef)
        {
-           printf("%x %x %x\n", block[0], block[1], block[2]);
+           printf("%x %x %x %x %x\n", block[0], block[1], block[2], block[3], block[4]);
            break;
        }
    }while (1);
-   //
-   char s[10]; //  it was [7]. I need to check if thast matters
-   //for (int i = 0; i < 50; i++)
-   do
-   {
-       sprintf(s, "%03i.jpg", i);
-       //printf("%s\n", s);
-       otp = fopen(s, "w");
-       fwrite(&block, 512, 1, otp);
-       i++;
-       do
-       {
-           if (fread(&block, 512, 1, inp) != 1)
-           {
-               fclose(inp);
-               fclose(otp);
-               return 0;
-           }
-           if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && block[3] >= 0xe0 && block[3] <= 0xef)
-           {
-               fclose(otp);
-               break;
-           }
-           fwrite(&block, 512, 1, otp);
-       } while (1);
-
-       //EEEEEOOOOOOOFFFFFF
-   } while(1);
-
-
-
-
-
-
-
-
-
-
-  /* fseek(fp, 1024, SEEK_CUR);
-
-for (int i = 0; i < 50; i++)
-   {
-
-
-
-       fread(&block, 512, 1, fp);
-       fwrite(&block, 512, 1, opt);
-       do
-       {
-           fread(&block, 512, 1, fp);
-           if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && block[3] >= 0xe0 && block[3] <= 0xef)
-           {
-               fclose(fp);
-               fclose(opt);
-               break;
-           }
-           fwrite(&block, 512, 1, opt);
-
-       }while(1);
-   }
-
-
-   if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && block[3] >= 0xe0 && block[3] <= 0xef)
-   {
-       printf("found a new JPEG file\n");
-       //fseek(fp, -512, SEEK_CUR);
-
-
-   }
-   */
-
 }
