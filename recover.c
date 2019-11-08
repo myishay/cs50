0b859cc9465a69e225e17cd10deac84403d07750
myishay <myishay@gmail.com>
2019-11-08 15:54:44 +0200

    Update recover.c

--- recover.c
+++ recover.c
@@ -1,46 +1,63 @@
-/**
- * recover.c
- *
- * Computer Science 50
- * Problem Set 4
- *
- * Recovers JPEGs from a forensic image.
- */
-
-/*int main(int argc, char* argv[])
-{
-    // TODO
-}
-*/
-#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-typedef uint8_t  BYTE;
-
-BYTE block[512];
-
-int main ()
+int main(int argc, char *argv[])
 {
-   FILE *inp;
-   FILE *otp;
-   //int c;
-   //int n = 0;
-   int i = 0;
+    // ensure proper usage
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: ./recover infile \n");
+        return 1;
+    }
 
-   inp = fopen("card.raw","r");
-   if(inp == NULL)
-   {
-      perror("Error in opening file");
-      return(-1);
-   }
-   //find the first JPEG start & insert it into 'block'
-   do
-   {
-       fread(&block, 512, 1, inp);
-       if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && block[3] >= 0xe0 && block[3] <= 0xef)
-       {
-           printf("%x %x %x %x %x\n", block[0], block[1], block[2], block[3], block[4]);
-           break;
-       }
-   }while (1);
+    //remember filenames
+    char *infile = argv[1];
+
+    // open input file
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", infile);
+        return 2;
+    }
+
+    typedef uint8_t BYTE;
+    BYTE buffer[512];
+    int JPEG_found = 0;
+    int JPEG_det = 0;
+    FILE *img = NULL;
+
+    while(fread(&buffer, 512, 1, inptr) != 0)
+    {
+        if (buffer[0] == 0xff &&
+            buffer[1] == 0xd8 &&
+            buffer[2] == 0xff &&
+            (buffer[3] & 0xf0) == 0xe0)
+            {
+                if (JPEG_found == 1)
+                {
+                    fclose(img);
+                }
+
+                JPEG_found = 1;
+                char filename[7];
+                sprintf(filename, "%03i.jpg", JPEG_det);
+                img = fopen(filename, "w");
+                JPEG_det++;
+            }
+
+        if (JPEG_found == 1)
+        {
+            fwrite(buffer, 512, 1, img);
+        }
+    }
+
+    fclose(img);
+
+    fclose(inptr);
+
+    printf("DONE! \n");
+
+    return 0;
 }
